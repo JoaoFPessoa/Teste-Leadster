94961ebfa66104e8ad2c74d0899f25959ea04774
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-06-29 14:41:53 -0300

    feat: add useClickOutside to close modals when clicking outside them

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,28 @@
+import { RefObject, useCallback, useEffect } from "react";
+
+interface IClickOutside {
+  /** Referencia do container que, quando clicado fora dele, vai executar a funcao passada no onClickOutside */
+  ref: RefObject<HTMLElement>;
+  /** Funcao que vai ser executada quando o clique acontecer fora do container de referencia */
+  onClickOutside: (event?: MouseEvent) => void;
+}
+function useClickOutside({ ref, onClickOutside }: IClickOutside) {
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      // Not trigger case when the element that have the class to be avoid is on screen
+
+      if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
+        onClickOutside(event);
+      }
+    },
+    [ref, onClickOutside]
+  );
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [handleClickOutside]);
+}
+
+export default useClickOutside;
