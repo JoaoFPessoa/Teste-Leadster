0bdaefc5e721aa53d6df14f14d2f1328f4e50dca
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-06-29 12:53:27 -0300

    style: add cards styles

---
+++ src/components/VideosMenu/Card/styles.ts
@@ -0,0 +1,48 @@
+import { styled } from "styled-components";
+
+export const Container = styled.article`
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  border-radius: 20px;
+  width: calc(33.99% - 15px);
+  position: relative;
+  height: 100%;
+  h4 {
+    padding: 1rem 2.5rem;
+  }
+  .thumbnail {
+    width: 100%;
+  }
+
+  &:hover {
+    transform: scale(1.04);
+    color: var(--primary);
+    transition: 0.2s;
+    .thumbnailContent {
+      background: var(--primary);
+      background-size: cover;
+      border-top-right-radius: 20px;
+      border-top-left-radius: 20px;
+    }
+    .thumbnail {
+      opacity: 0.6;
+    }
+  }
+
+  button {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    z-index: 2;
+    cursor: pointer;
+
+    img {
+      width: 80px;
+      height: 80px;
+    }
+  }
+`;
