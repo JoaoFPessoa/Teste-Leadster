6d2e9ce2f926f33742d11834e94a8a22fe37b676
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-06-29 15:01:25 -0300

    styles: add mediaQuery

--- src/components/VideosMenu/Filter/styles.ts
+++ src/components/VideosMenu/Filter/styles.ts
@@ -6,16 +6,39 @@ export const Container = styled.div`
   justify-content: space-between;
   border-bottom: 1px solid var(--border-color);
   padding-bottom: 1rem;
+
+  /* @media (min-width: 865px) and (max-width: 1200px) { */
+  @media (max-width: 1200px) {
+    flex-direction: column;
+    div:first-of-type {
+      margin-bottom: 1rem;
+    }
+  }
+  @media (max-width: 480px) {
+  }
 `;
 export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   button {
     margin-right: 8px;
+    font-size: 15px;
+    @media (max-width: 480px) {
+      width: 120px;
+    }
+  }
+  @media (max-width: 865px) {
+    align-items: center;
+    justify-content: center;
   }
 `;
 
 export const OrdinationContainer = styled.div`
   display: flex;
   align-items: center;
+  @media (max-width: 480px) {
+    flex-direction: column;
+  }
   h3 {
     font-size: 16px;
     margin-right: 8px;
